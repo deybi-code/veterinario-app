3d1ab51e49ff8a22db5cc27c77edbb132ac4733f
deybi-code <deybigavidia0@gmail.com>
2026-06-03 13:02:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 const { spawn } = require('child_process');
 spawn('php', ['-S', '0.0.0.0:' + process.env.PORT, '-t', 'public'], { stdio: 'inherit' });
+import { spawn } from 'child_process';
+spawn('php', ['-S', '0.0.0.0:' + process.env.PORT, '-t', 'public'], { stdio: 'inherit' });
